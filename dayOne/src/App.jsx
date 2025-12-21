c9d42b5575d628db4574a2c58b3d22f984c70728
Priyanshu Kumar <priyanshu3960@gmail.com>
2025-12-21 22:43:13 +0530

    Initial commit

---
+++ dayOne/src/App.jsx
@@ -0,0 +1,14 @@
+
+const App = () => {
+  return (
+    <>
+    <div className="parent">
+    <h1>HEllo</h1>
+    <h1>Helllo2</h1>
+    </div>
+    <div className="chacha"></div>
+    </>
+  )
+}
+
+export default App
