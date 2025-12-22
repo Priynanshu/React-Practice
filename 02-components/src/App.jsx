3bf02fcf659aed75e98c4c4a81b5265a00c7c119
Priyanshu Kumar <priyanshu3960@gmail.com>
2025-12-22 17:43:43 +0530

    Components

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from "./components/card.jsx";
+const App = () => {
+  return (
+    <div>
+     
+     <Card />
+     <Card />
+     <Card /><Card />
+
+     <Card />
+    </div>
+    
+  )
+}
+
+export default App
---
+++ 02-components/src/components/card.jsx
@@ -0,0 +1,5 @@
+function Card() {
+    return "hi this is card fun in components folder"
+}
+
+export default Card;
